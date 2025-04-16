2297009e391f60707e99d5fef155228d27ac2b27
Md Imam Hosen <mimam22.cse@bu.ac.bd>
2025-04-17 02:23:00 +0600

    Bike Sevice Center Backend

--- dist/app.js
+++ dist/app.js
@@ -12,7 +12,6 @@ const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json({ limit: '50mb' }));
 app.use(express_1.default.json());
-app.use(express_1.default.static('public'));
 app.get('/', (req, res) => {
     res.send('Hello from Express!');
 });
--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,6 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.json());
 
-app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
 });
